b88f908605adc27adf83fd3eea3f548d09a25e89
Dan Funk <daniel.h.funk@gmail.com>
2018-12-28 17:34:38 -0500

    Lots more word smithing, and a smattering of animations.

--- src/app/case-studies/case-studies.component.ts
+++ src/app/case-studies/case-studies.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
 import { CaseStudy } from '../interfaces';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-case-studies',
   templateUrl: './case-studies.component.html',
-  styleUrls: ['./case-studies.component.scss']
+  styleUrls: ['./case-studies.component.scss'],
+  animations: [
+    trigger('activate', [
+      state('active', style({
+        opacity: 1
+      })),
+      state('inactive', style({
+        opacity: 0.5,
+        transform: 'scale(.75)',
+      })),
+      transition('* <=> *', animate('200ms ease-in')),
+    ]),
+    trigger('visible', [
+      state('*', style({
+        opacity: 1,
+      })),
+      state('void', style({
+        opacity: 0,
+      })),
+      transition('* <=> *', animate('200ms ease-in')),
+    ])
+
+  ]
 })
 export class CaseStudiesComponent implements OnInit {
   caseStudies: CaseStudy[];
   caseStudy: CaseStudy;
+  showCase = false;
 
   constructor(
     private api: ApiService
@@ -18,10 +42,12 @@ export class CaseStudiesComponent implements OnInit {
   ngOnInit() {
     this.api.getCaseStudies().subscribe(studies => {
       this.caseStudies = studies;
+      this.caseStudy = this.caseStudies[0];
     });
   }
 
   setStudyView(study: CaseStudy) {
+    this.showCase = true;
     this.caseStudy = study;
   }
 
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      state('void', style({
+        opacity: 0
+      })),
+      state('*', style({
+        opacity: 100
+      })),
+      transition('void <=> *', animate('1000ms ease-in')),
+    ])
+  ]
 })
 export class HeaderComponent implements OnInit {
 
