e812fbb4ffdee35cbb70ae5d79cb71364a02c5c3
Aaron Louie <aaron@sartography.com>
2019-10-14 12:21:12 -0400

    Adds easing function

--- src/app/animation.ts
+++ src/app/animation.ts
@@ -7,25 +7,34 @@ import {
   animate,
   style,
   query
-} from "@angular/animations";
+} from '@angular/animations';
 import { EasingLogic } from 'ngx-page-scroll-core';
 
 export const fadeOut = animation(
   [
-    style({ opacity: "{{ start }}", zIndex: "{{zIndex}}" }),
-    animate("{{ time }}", style({ opacity: "{{ end }}" }))
+    style({ opacity: '{{ start }}', zIndex: '{{zIndex}}' }),
+    animate('{{ time }}', style({ opacity: '{{ end }}' }))
   ],
-  { params: { time: "1000ms", start: 0, end: 1, zIndex: 0 } }
+  { params: { time: '1000ms', start: 0, end: 1, zIndex: 0 } }
 );
 
 export const fadeIn = animation(
   [
-    style({ opacity: "{{ start }}", zIndex: "{{zIndex}}" }),
-    animate("{{ time }}", style({ opacity: "{{ end }}" }))
+    style({ opacity: '{{ start }}', zIndex: '{{zIndex}}' }),
+    animate('{{ time }}', style({ opacity: '{{ end }}' }))
   ],
-  { params: { time: "1000ms", start: 1, end: 0, zIndex: 0 } }
+  { params: { time: '1000ms', start: 1, end: 0, zIndex: 0 } }
 );
 
-export const easingFn: EasingLogic = (t: number, b: number, c: number, d: number): number => {
-  return ((t/=d/2) < 1) ? ((c/2)*t*t*t*t+b) : (-c/2)*((t-=2)*t*t*t-2)+b;
+export const easingFn: EasingLogic = (
+  t: number,
+  b: number,
+  c: number,
+  d: number
+): number => {
+  if ((t /= d / 2) < 1) {
+    return (c / 2) * t * t * t * t + b;
+  } else {
+    return (-c / 2) * ((t -= 2) * t * t * t - 2) + b;
+  }
 };
