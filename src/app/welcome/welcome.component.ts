353448854517b9e3528dab6d8ed375a7c304382c
Aaron Louie <aaron@sartography.com>
2019-10-14 12:20:51 -0400

    Changes double quotes to single.

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef, HostListener } from "@angular/core";
+import { Component, OnInit, ElementRef, HostListener } from '@angular/core';
 import {
   animate,
   state,
@@ -6,50 +6,50 @@ import {
   transition,
   trigger,
   useAnimation
-} from "@angular/animations";
-import { fadeIn, fadeOut } from "../animation";
+} from '@angular/animations';
+import { fadeIn, fadeOut } from '../animation';
 
 @Component({
-  selector: "app-welcome",
-  templateUrl: "./welcome.component.html",
-  styleUrls: ["./welcome.component.scss"],
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.scss'],
   animations: [
-    trigger("fadeInOut", [
-      transition("void <=> *", useAnimation(fadeIn)),
-      transition("* <=> void", useAnimation(fadeOut))
+    trigger('fadeInOut', [
+      transition('void <=> *', useAnimation(fadeIn)),
+      transition('* <=> void', useAnimation(fadeOut))
     ]),
-    trigger("scrollAnimation", [
+    trigger('scrollAnimation', [
       state(
-        "show",
+        'show',
         style({
           opacity: 1,
           zIndex: 0
         })
       ),
       state(
-        "hide",
+        'hide',
         style({
           opacity: 0,
           zIndex: 0
         })
       ),
-      transition("show => hide", animate("700ms ease-out")),
-      transition("hide => show", animate("700ms ease-in"))
+      transition('show => hide', animate('700ms ease-out')),
+      transition('hide => show', animate('700ms ease-in'))
     ])
   ]
 })
 export class WelcomeComponent implements OnInit {
-  state = "hide";
+  state = 'hide';
 
   constructor(public el: ElementRef) {}
 
   ngOnInit() {}
 
-  @HostListener("window:scroll", ["$event"])
+  @HostListener('window:scroll', ['$event'])
   checkScroll() {
     const hOffset = window.innerHeight / 2;
     const componentPosition = this.el.nativeElement.offsetTop;
     const scrollPosition = window.pageYOffset + hOffset;
-    this.state = (scrollPosition >= componentPosition) ? "show" : "hide";
+    this.state = scrollPosition >= componentPosition ? 'show' : 'hide';
   }
 }
