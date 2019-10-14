5756615159a7725fc03659cd1a0dab911b6122f1
Aaron Louie <aaron@sartography.com>
2019-10-14 12:22:28 -0400

    Moves menu component into app component. Uses new scrollSpy directive to move selected section indicator.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, ChangeDetectionStrategy, Component, QueryList, ViewChildren} from '@angular/core';
+import {HeaderComponent} from './header/header.component';
+import {WelcomeComponent} from './welcome/welcome.component';
+import {AboutUsComponent} from './about-us/about-us.component';
+import {LogoValuesComponent} from './logo-values/logo-values.component';
+import {TeamComponent} from './team/team.component';
+import {CaseStudiesComponent} from './case-studies/case-studies.component';
+import {ContactUsComponent} from './contact-us/contact-us.component';
+
+interface MenuLink {
+  id: string;
+  label: string;
+  component: any;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  menuLinks: MenuLink[] = [
+    {id: 'header', label: 'Home', component: HeaderComponent},
+    {id: 'welcome', label: 'Intro', component: WelcomeComponent},
+    {id: 'about_us', label: 'About Us', component: AboutUsComponent},
+    {id: 'logo_values', label: 'Our Values', component: LogoValuesComponent},
+    {id: 'team', label: 'Our Team', component: TeamComponent},
+    {id: 'case_studies', label: 'Projects', component: CaseStudiesComponent},
+    {id: 'contact_us', label: 'Contact', component: ContactUsComponent},
+  ];
+
+  @ViewChildren('scrollSpySection') scrollSpySections: QueryList<any>;
   title = 'Sartography';
+  currentSection = 'header';
+  activeLinkTop: number;
+  showIndicator = false;
+
+  constructor() {}
+
+  ngAfterViewInit() {
+  }
+
+  moveSelectedIndicator(linkId: string) {
+    this.currentSection = linkId;
+    const activeLinkEl = document.getElementById(`${this.currentSection}_menu_link`);
+    this.activeLinkTop = activeLinkEl.offsetTop;
+  }
+
+  onMenuClick(linkId: string) {
+    this.showIndicator = true;
+    this.onSectionChange(linkId);
+  }
+
+  onSectionChange(sectionId: string) {
+    this.currentSection = sectionId;
+    this.moveSelectedIndicator(this.currentSection);
+    console.log('section change to:', sectionId);
+  }
+
+  scrollTo(section) {
+    document.querySelector('#' + section)
+    .scrollIntoView();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,24 @@
-import { HttpClientModule } from "@angular/common/http";
-import { NgModule } from "@angular/core";
-import { FlexLayoutModule } from "@angular/flex-layout";
-import {
-  MatButtonModule,
-  MatCardModule,
-  MatIconModule,
-  MatMenuModule,
-  MatTabsModule,
-  MatToolbarModule
-} from "@angular/material";
-import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { InlineSVGModule } from "ng-inline-svg";
-import { NgxPageScrollModule } from "ngx-page-scroll";
-import { AboutUsComponent } from "./about-us/about-us.component";
-import { ApiService } from "./api.service";
-import { AppComponent } from "./app.component";
-import { CaseStudiesComponent } from "./case-studies/case-studies.component";
-import { ContactUsComponent } from "./contact-us/contact-us.component";
-import { FooterComponent } from "./footer/footer.component";
-import { HeaderComponent } from "./header/header.component";
-import { MenuComponent } from "./menu/menu.component";
-import { TeamComponent } from "./team/team.component";
-import { WelcomeComponent } from "./welcome/welcome.component";
-import { NgxPageScrollCoreModule } from 'ngx-page-scroll-core';
-import { easingFn } from './animation';
-
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatButtonModule, MatCardModule, MatIconModule, MatMenuModule, MatTabsModule, MatToolbarModule} from '@angular/material';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {InlineSVGModule} from 'ng-inline-svg';
+import {NgxPageScrollModule} from 'ngx-page-scroll';
+import {AboutUsComponent} from './about-us/about-us.component';
+import {ApiService} from './api.service';
+import {AppComponent} from './app.component';
+import {CaseStudiesComponent} from './case-studies/case-studies.component';
+import {ContactUsComponent} from './contact-us/contact-us.component';
+import {FooterComponent} from './footer/footer.component';
+import {HeaderComponent} from './header/header.component';
+import {TeamComponent} from './team/team.component';
+import {WelcomeComponent} from './welcome/welcome.component';
+import {NgxPageScrollCoreModule} from 'ngx-page-scroll-core';
+import {easingFn} from './animation';
+import {LogoValuesComponent} from './logo-values/logo-values.component';
+import {ScrollSpyDirective} from './scroll-spy.directive';
 
 @NgModule({
   declarations: [
@@ -35,9 +28,10 @@ import { easingFn } from './animation';
     ContactUsComponent,
     FooterComponent,
     HeaderComponent,
-    MenuComponent,
+    LogoValuesComponent,
+    ScrollSpyDirective,
     TeamComponent,
-    WelcomeComponent
+    WelcomeComponent,
   ],
   imports: [
     BrowserAnimationsModule,
@@ -55,6 +49,16 @@ import { easingFn } from './animation';
     NgxPageScrollCoreModule.forRoot({easingLogic: easingFn})
   ],
   providers: [ApiService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [
+    AboutUsComponent,
+    CaseStudiesComponent,
+    ContactUsComponent,
+    HeaderComponent,
+    LogoValuesComponent,
+    TeamComponent,
+    WelcomeComponent,
+  ]
 })
-export class AppModule {}
+export class AppModule {
+}
