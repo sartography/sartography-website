d1b62e00b10d23fc40b76237ba91f6110c3b9045
Aaron Louie <aaron@sartography.com>
2019-10-16 17:14:53 -0400

    Gets content from API. Highlights appropriate logo element on hover of related content section.

--- src/app/logo-values/logo-values.component.ts
+++ src/app/logo-values/logo-values.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {BrandValue} from '../interfaces';
+import {ApiService} from '../api.service';
+import {element} from 'protractor';
 
 @Component({
   selector: 'app-logo-values',
@@ -6,7 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./logo-values.component.scss']
 })
 export class LogoValuesComponent implements OnInit {
-  constructor() {}
+  brandValuesLeft: BrandValue[];
+  brandValuesRight: BrandValue[];
+
+  constructor(private api: ApiService) {
+    this.api.getBrandValues().subscribe(brandValues => {
+      const midIndex = Math.floor(brandValues.length / 2);
+      this.brandValuesLeft = brandValues.slice(0, midIndex);
+      this.brandValuesRight = brandValues.slice(midIndex);
+    });
+  }
 
   ngOnInit() {}
+
+  highlight(elementId: string, on: boolean) {
+    if (on) {
+      document.getElementById(elementId).classList.add('highlight');
+    } else {
+      document.getElementById(elementId).classList.remove('highlight');
+    }
+  }
 }
