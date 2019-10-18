53d643172751785aa3b69d6fdaf072e88c943d67
Aaron Louie <aaron@sartography.com>
2019-10-18 16:29:32 -0400

    Redesigns footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,16 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, HostListener, OnInit} from '@angular/core';
+import {getScrollState} from '../util';
+import {animations} from '../animation';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
+  animations: animations
 })
 export class FooterComponent implements OnInit {
-
+  state = 'hide';
   today: number = Date.now();
-  constructor() { }
+  constructor(
+    public el: ElementRef
+  ) { }
 
   ngOnInit() {
   }
 
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    this.state = getScrollState(this.el);
+  }
 }
