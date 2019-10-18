7be75dbd54e22a262aa5c9b4cf525e2290ad3122
Aaron Louie <aaron@sartography.com>
2019-10-18 16:26:58 -0400

    Adds team member component so we can animate on scroll for each one

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -1,24 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, HostListener, OnInit} from '@angular/core';
 import { TeamMember } from '../interfaces';
 import { ApiService } from '../api.service';
-import {isEven} from '../util';
+import {getScrollState, isEven} from '../util';
+import {animations} from '../animation';
 
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
-  styleUrls: ['./team.component.scss']
+  styleUrls: ['./team.component.scss'],
+  animations: animations
 })
 export class TeamComponent implements OnInit {
   teamMembers: TeamMember[];
   isEven = isEven;
+  state = 'hide';
 
   constructor(
-    private api: ApiService
+    private api: ApiService,
+    public el: ElementRef
   ) { }
 
   ngOnInit() {
     this.api.getTeamMembers().subscribe(team => {
       this.teamMembers = team;
     });
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    this.state = getScrollState(this.el);
   }
 }
